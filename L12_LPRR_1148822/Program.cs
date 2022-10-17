7eef78a8daef6d246379e48acdb39988a36c15b5
Estudiante <Estudiante@T203-16-2022>
2022-10-17 21:59:01 +0200

    Agregar archivos de proyecto.

---
+++ L12_LPRR_1148822/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] notas = new int[14];
+
+        for (int i = 0; i < 14; i++)
+        {
+            Console.WriteLine("Ingrese nota ");
+            notas[i] = int.Parse(Console.ReadLine());
+        }
+
+        double promedio = 0;
+        double suma = 0;
+        for (int i = 0; i < 14; i++)
+        {
+            suma = suma + notas[i];
+        }
+
+        promedio = suma / 14;
+        Console.WriteLine("El promedio es " + promedio);
+    }
+}
